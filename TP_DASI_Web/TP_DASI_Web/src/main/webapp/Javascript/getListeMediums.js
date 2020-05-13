bf1e9ff6cb0bdbefcbd466fa39dde8b0e2cce075
Emma <emmaneiss2@gmail.com>
2020-05-13 17:06:04 +0200

    Liste mediums :white_check_mark:
    Fonctionnalités basiques

--- TP_DASI_Web/TP_DASI_Web/src/main/webapp/Javascript/getListeMediums.js
+++ TP_DASI_Web/TP_DASI_Web/src/main/webapp/Javascript/getListeMediums.js
@@ -7,13 +7,16 @@
 //import fr.insalyon.dasi.metier.service.Service;
 
 
-$(document).ready(
+$(document)
+    
+    .ready(
         function getListeMediums() {
 
-    console.log("======================\nclic sur le bouton 'nos mediums', appel à getListeMediums (JS)\n=========================="); // LOG dans Console Javascript
+    console.log("==========================\nclic sur le bouton 'nos mediums', appel à getListeMediums (JS)\n=========================="); // LOG dans Console Javascript
     var text = "";
 
-    if (navigator.cookieEnabled == true) {
+    // bail inutile pour vérifier que tout va bien
+    if (navigator.cookieEnabled === true) {
         text = "Cookies are enabled.";
     } else {
         text = "Cookies are not enabled.";
@@ -26,24 +29,35 @@ $(document).ready(
         url: './MainController',
         method: 'GET',
         data: {
-            todo: 'getListeMediums',
+            todo: 'getListeMediums'
         },
         dataType: 'json'
     })
     .done( function (response) { // Fonction appelée en cas d'appel AJAX réussi
         console.log('Response',response); // LOG dans Console Javascript
-        // if (response.success) {
+        if (response.success) {
             
-            console.log("GG WP Json bien recu")
-            //$('#notification').html("Connexion OK");  // Message pour le paragraphe de notification
-            // TODO: afficher les informations du client dans la notification
-            // Exemple: Connexion de Ada Lovelace (ID 1)
-//            var client = response.client;
-//            $('#notification').html("Connexion de " + client.prenom + " " + client.nom + " (ID " + client.id + ")");  // Message pour le paragraphe de notification
-//        }
-//        else {
-//            $('#notification').html("Erreur de Connexion"); // Message pour le paragraphe de notification
-//        }
+            console.log("GG WP Json bien recu");
+            
+            var htmlContent = "<table id='listeMediums'>";
+            
+            for (var i = 0; i < response.liste.length; i++){
+                
+                var medium = response.liste[i];
+                htmlContent += "<tr>";
+                htmlContent += "<td>"+medium.denom+"</td><td>"+medium.presentation+"</td></tr>";
+            }
+            
+            htmlContent += "</table>";
+            
+            document.getElementById("listeMediums").innerHTML = htmlContent;
+    
+        }
+        else {
+            
+            document.getElementById("listeMediums").innerHTML = "Erreur... :(";
+            $('#notification').html("Erreur de Connexion"); // Message pour le paragraphe de notification
+        }
     })
     .fail( function (error) { // Fonction appelée en cas d'erreur lors de l'appel AJAX
         console.log('Error',error); // LOG dans Console Javascript
