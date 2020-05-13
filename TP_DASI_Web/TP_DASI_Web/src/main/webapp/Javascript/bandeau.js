eafc8ca9234d77928368eb2590a17adde39d04fb
Yumahey <39220591+Yumahey@users.noreply.github.com>
2020-05-13 18:01:45 +0200

    joli bandeau

--- TP_DASI_Web/TP_DASI_Web/src/main/webapp/Javascript/bandeau.js
+++ TP_DASI_Web/TP_DASI_Web/src/main/webapp/Javascript/bandeau.js
@@ -5,9 +5,10 @@
  */
 
 $(document).ready(
-        function getListeMediums() {
+        function bandeau() {
 
     console.log("======================\nchargement d'une page a bandeau, appel à bandeau (JS)\n=========================="); // LOG dans Console Javascript
     
+    document.getElementById("menu").innerHTML += "<a id=logo style='color:White;font-size:500%;' href='index.html' title='Retour à la page d&apos;accueil'>Predicti'IF</a>";
 })
 
