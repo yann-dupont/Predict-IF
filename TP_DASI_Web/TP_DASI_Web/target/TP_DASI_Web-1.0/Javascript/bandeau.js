e356477dceb5d01b8285ef7bdf569f6302d2323b
Yumahey <39220591+Yumahey@users.noreply.github.com>
2020-05-18 11:01:18 +0200

    gestion session v1

--- TP_DASI_Web/TP_DASI_Web/target/TP_DASI_Web-1.0/Javascript/bandeau.js
+++ TP_DASI_Web/TP_DASI_Web/target/TP_DASI_Web-1.0/Javascript/bandeau.js
@@ -4,11 +4,20 @@
  * and open the template in the editor.
  */
 
+//import {getCookie} from "./Javascript/gestionCookies.js";
+
 $(document).ready(
         function bandeau() {
 
     console.log("==========================\nchargement d'une page a bandeau, appel à bandeau (JS)\n=========================="); // LOG dans Console Javascript
     
     document.getElementById("menu").innerHTML += "<a id=logo style='color:White;font-size:500%;' href='index.html' title='Retour à la page d&apos;accueil'>Predicti'IF</a>";
-})
+    var userId = getCookie("user");
+    if(userId==="")
+    {
+        document.getElementById("menu").innerHTML += "<div id=userWelcome style='color:White;font-size:200%;'>Pas connecté :(</div>";
+    } else {
+        document.getElementById("menu").innerHTML += "<div id=userWelcome style='color:White;font-size:200%;'>ID de l'utilisateur : " + userId + "</div>";
+    }
+});
 
---
+++ TP_DASI_Web/TP_DASI_Web/target/TP_DASI_Web-1.0/Javascript/gestionCookies.js
@@ -0,0 +1,23 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+// trouvé sur https://www.w3schools.com/js/js_cookies.asp
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
