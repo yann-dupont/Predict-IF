155660f4d49940bf0dd4b1ae60deb6192fc92657
Emma <emmaneiss2@gmail.com>
2020-05-19 11:28:34 +0200

    Upgrades avec la session

---
+++ TP_DASI_Web/TP_DASI_Web/target/TP_DASI_Web-1.0/Javascript/connexion.js
@@ -0,0 +1,127 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+// TODO : mettre dans un autre JS séparé quand on saura importer correctement
+/*
+ * Source de la fonction GetURLParameter :
+ * http://www.jquerybyexample.net/2012/06/get-url-parameters-using-jquery.html
+ */
+function GetURLParameter(sParam)
+{
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++)
+    {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == sParam)
+        {
+            return sParameterName[1];
+        }
+    }
+    
+    return null;
+}
+
+
+
+$(document).ready( function () {
+    $('#bouton-connexion').on( 'click', function () { // Fonction appelée lors du clic sur le bouton
+
+        console.log("clic sur le bouton de connexion"); // LOG dans Console Javascript
+        $('#notification').html("Connexion..."); // Message pour le paragraphe de notification
+
+        // Récupération de la valeur des champs du formulaire
+        var champLogin = $('#champ-login').val();
+        var champPassword = $('#champ-password').val();
+
+        // Appel AJAX pour la connexion client
+        $.ajax({
+            url: './MainController',
+            method: 'POST',
+            data: {
+                todo: 'connecterClient',
+                login: champLogin,
+                password: champPassword
+            },
+            dataType: 'json'
+        })
+        .done( function (response) { // Fonction appelée en cas d'appel AJAX réussi
+            console.log('Response : ',response); // LOG dans Console Javascript
+            if (response.connexion) {
+                //$('#notification').html("Connexion OK");  // Message pour le paragraphe de notification
+                // TODO: afficher les informations du client dans la notification
+                // Exemple: Connexion de Ada Lovelace (ID 1)
+                var client = response.client;
+                $('#notification').html("Connexion du client " + client.prenom + " " + client.nom + " (ID " + client.id + ")");  // Message pour le paragraphe de notification
+                document.cookie = "user=" + client.id;
+                
+                console.log("Allo ? Le bouton connexion ??");
+                
+                var redir = GetURLParameter("prev");
+                console.log("Redir : ", redir);
+                if(redir === null){
+                    window.location = "profilClient.html";  // test -> mettre sur index
+                }else{
+                    window.location = redir;
+                }
+            }
+            else {
+                $('#notification').html("Erreur de Connexion"); // Message pour le paragraphe de notification
+            }
+        })
+        .fail( function (error) { // Fonction appelée en cas d'erreur lors de l'appel AJAX
+            console.log('Error',error); // LOG dans Console Javascript
+            alert("Erreur lors de l'appel AJAX");
+        })
+        .always( function () { // Fonction toujours appelée
+
+        });
+
+        // Appel AJAX pour la connexion employé
+        $.ajax({
+            url: './MainController',
+            method: 'POST',
+            data: {
+                todo: 'connecterEmploye',
+                login: champLogin,
+                password: champPassword
+            },
+            dataType: 'json'
+        })
+        .done( function (response) { // Fonction appelée en cas d'appel AJAX réussi
+            console.log('Response',response); // LOG dans Console Javascript
+            if (response.connexion) {
+                //$('#notification').html("Connexion OK");  // Message pour le paragraphe de notification
+                // TODO: afficher les informations du client dans la notification
+                // Exemple: Connexion de Ada Lovelace (ID 1)
+                var employe = response.employe;
+                $('#notification').html("Connexion de l'employé " + employe.prenom + " " + employe.nom + " (ID " + employe.id + ")");  // Message pour le paragraphe de notification
+                document.cookie = "user=" + employe.id;
+                
+                var redir = GetURLParameter("prev");
+                console.log("Redir : ", redir);
+                if(redir === null){
+                    window.location = "profilClient.html";  // test -> mettre sur index
+                }else{
+                    window.location = redir;
+                }
+                
+            }
+            else {
+                $('#notification').html("Erreur de Connexion"); // Message pour le paragraphe de notification
+            }
+        })
+        .fail( function (error) { // Fonction appelée en cas d'erreur lors de l'appel AJAX
+            console.log('Error',error); // LOG dans Console Javascript
+            alert("Erreur lors de l'appel AJAX1");
+        })
+        .always( function () { // Fonction toujours appelée
+
+        });
+
+    });
+});
--- TP_DASI_Web/TP_DASI_Web/target/TP_DASI_Web-1.0/Javascript/getListeMediums.js
+++ TP_DASI_Web/TP_DASI_Web/target/TP_DASI_Web-1.0/Javascript/getListeMediums.js
@@ -74,44 +74,56 @@ $(document)
     });
 });
 
+
+// appelée quand on clique sur "contacter"
 function contacterMedium(id){
     
     if(confirm("Contacter ce médium ?")){
-        console.log("Validation contact");
         
-        // Appel AJAX
-        $.ajax({
-            
-            url: './MainController',
-            method: 'GET',
-            data: {
-                todo: 'contacterMedium',
-                id: id
-            },
-            dataType: 'json'
-        })
-        .done( function (response) { // Fonction appelée en cas d'appel AJAX réussi
-            console.log("Done");
-            console.log("Appel AJAX pour contacter le medium réussi");
-            console.log('Response',response); // LOG dans Console Javascript
-            
-            if(response.success){
-                console.log("Youpi le medium a été contacté");
-                window.location = response.url;
-            }else{
-                alert("Désolé, ce medium n'est pas disponible pour le moment...\nRetentez votre chance plus tard !");
-            }
+        var userId = getCookie("user");
+        console.log('ID user : '+userId);
         
-        })
-        .fail( function (error) { // Fonction appelée en cas d'erreur lors de l'appel AJAX
-            console.log('Error',error); // LOG dans Console Javascript
-            alert("Erreur lors de l'appel AJAX de contacterMedium");
-        })
-        .always( function () { // Fonction toujours appelée
-            console.log("Always");
-        });
-        
-    }else{
+        if(userId==="")
+        {
+            window.location = "connexion.html?prev=listeMediums.html";
+        }else{  // utilisateur connecté
+            // TODO : tester si c'est un employé, si oui refuser
+            console.log("Validation contact");
+
+            // Appel AJAX
+            $.ajax({
+
+                url: './MainController',
+                method: 'GET',
+                data: {
+                    todo: 'contacterMedium',
+                    id: id
+                },
+                dataType: 'json'
+            })
+            .done( function (response) { // Fonction appelée en cas d'appel AJAX réussi
+                console.log("Done");
+                console.log("Appel AJAX pour contacter le medium réussi");
+                console.log('Response',response); // LOG dans Console Javascript
+
+                if(response.success){
+                    console.log("Youpi le medium a été contacté");
+                    window.location = response.url;
+                }else{
+                    alert("Désolé, ce medium n'est pas disponible pour le moment...\nRetentez votre chance plus tard !");
+                }
+
+            })
+            .fail( function (error) { // Fonction appelée en cas d'erreur lors de l'appel AJAX
+                console.log('Error',error); // LOG dans Console Javascript
+                alert("Erreur lors de l'appel AJAX de contacterMedium");
+            })
+            .always( function () { // Fonction toujours appelée
+                console.log("Always");
+            });            
+        }
+
+    }else{      // "annuler" dans la boite de dialogue de confirmation
         console.log("Annulé");
     }
 }
