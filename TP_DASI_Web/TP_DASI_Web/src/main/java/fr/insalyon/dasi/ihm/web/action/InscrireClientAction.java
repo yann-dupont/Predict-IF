5cb7649f6680ba6229939afafa7d4524d7db4105
Emma <emmaneiss2@gmail.com>
2020-05-20 17:34:50 +0200

    Inscription toute fumante

---
+++ TP_DASI_Web/TP_DASI_Web/src/main/java/fr/insalyon/dasi/ihm/web/action/InscrireClientAction.java
@@ -0,0 +1,42 @@
+package fr.insalyon.dasi.ihm.web.action;
+
+import fr.insalyon.dasi.metier.modele.Client;
+import fr.insalyon.dasi.metier.service.Service;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author DASI Team
+ */
+public class InscrireClientAction extends Action {
+
+    @Override
+    public void executer(HttpServletRequest request) {
+        
+        String nom = request.getParameter("nom");
+        String prenom = request.getParameter("prenom");
+        String mail = request.getParameter("mail");
+        String password = request.getParameter("password");
+        String tel = request.getParameter("tel");
+        int jour = Integer.parseInt(request.getParameter("jour"));
+        int mois = Integer.parseInt(request.getParameter("mois"));
+        int annee = Integer.parseInt(request.getParameter("annee"));
+        
+        System.out.println("Jour, mois, annee : "+jour+mois+annee);
+
+        Client c = new Client(nom, prenom, mail, password, tel, jour, mois, annee);
+        Service service = new Service();
+        Long res = service.inscrireClient(c);
+
+        if (res != null) {
+            request.setAttribute("success", true);
+            System.out.println("Succès InscrireClientAction");
+        }
+        else {
+            request.setAttribute("success", false);
+            System.out.println("Echec InscrireClientAction... :'(");
+        }
+    }
+    
+}
--- TP_DASI_Web/TP_DASI_Web/src/main/webapp/Javascript/inscription.js
+++ TP_DASI_Web/TP_DASI_Web/src/main/webapp/Javascript/inscription.js
@@ -28,27 +28,8 @@ function GetURLParameter(sParam)
 
 $(document).ready( function () {
     
-
     /*
-     * Bind le bouton enter sur le champ X ?
-     */
-    // Get the input field
-//    var input = document.getElementById("champ-password");
-//    console.log(input);
-//
-//    // Execute a function when the user releases a key on the keyboard
-//    input.addEventListener("keyup", function(event) {
-//      // Number 13 is the "Enter" key on the keyboard
-//      if (event.keyCode === 13) {
-//        // Cancel the default action, if needed
-//        event.preventDefault();
-//        // Trigger the button element with a click
-//        document.getElementById("bouton-connexion").click();
-//      }
-//    }); 
-
-    /*
-     * Comportement du bouton connexion (ou enter sur champ mdp)
+     * Comportement du bouton connexion (ou enter ?)
      */
     $('#bouton-confirmer').on( 'click', function () { // Fonction appelée lors du clic sur le bouton
 
@@ -56,8 +37,14 @@ $(document).ready( function () {
 
         // Récupération de la valeur des champs du formulaire
         var nom = $('#nom').val();
-        var prenom = $('#nom').val();
+        var prenom = $('#prenom').val();
         var mail = $('#mail').val();
+        var mdp = $('#password').val();
+        var jour = $('#ddn').val().substring(8,10);
+        var mois = $('#ddn').val().substring(5,7);
+        var annee = $('#ddn').val().substring(0,4);
+        
+        console.log("jour, mois, année : ",jour, mois, annee);
 
         // Appel AJAX pour l'inscription client
         $.ajax({
@@ -67,39 +54,39 @@ $(document).ready( function () {
                 todo: 'inscrireClient',
                 nom: nom,
                 prenom: prenom,
-                mail: mail
+                mail: mail,
+                password: mdp,
+                jour: jour,
+                mois: mois,
+                annee: annee
             },
             dataType: 'json'
         })
         .done( function (response) { // Fonction appelée en cas d'appel AJAX réussi
             console.log('Response : ',response); // LOG dans Console Javascript
-            if (response.connexion) {
-                //$('#notification').html("Connexion OK");  // Message pour le paragraphe de notification
-                // TODO: afficher les informations du client dans la notification
-                // Exemple: Connexion de Ada Lovelace (ID 1)
-                var client = response.client;
-                $('#notification').html("Connexion du client " + client.prenom + " " + client.nom + " (ID " + client.id + ")");  // Message pour le paragraphe de notification
-                document.cookie = "user=" + client.id;
-                
-                console.log("Allo ? Le bouton connexion ??");
+            if (response.success) {
+                                
+                console.log("Inscription reussie !");
+                $('#notification').html("Inscription terminée !");
                 
                 var redir = GetURLParameter("prev");
                 console.log("Redir : ", redir);
                 if(redir === null){
-                    window.location = "index.html";  // test -> mettre sur index
+                    window.location = "index.html";
                 }else{
                     window.location = redir;
                 }
                 return;
             }
             else {
-                $('#notification').html("Erreur d'inscription"); // Message pour le paragraphe de notification
+                $('#notification').html("Erreur d'inscription :("); // Message pour le paragraphe de notification
             }
         })
-        .fail( function (error) { // Fonction appelée en cas d'erreur lors de l'appel AJAX
-            console.log('Error',error); // LOG dans Console Javascript
-            alert("Erreur lors de l'appel AJAX");
-        })
+        .fail( function (xhr, status, error) { // Fonction appelée en cas d'erreur lors de l'appel AJAX
+
+            console.log(xhr.responseText);
+//            alert("Erreur lors de l'appel AJAX");
+        });
 
     });
 });
